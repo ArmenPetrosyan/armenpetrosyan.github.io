7ddca34be6fa49b651100a708f6132994c9917e0
arnie <thebraincrasher@gmail.com>
2017-04-13 00:29:15 +0300

    UPD: page updated add sorting

--- js/main.js
+++ js/main.js
@@ -2,58 +2,87 @@ class MovieFinder {
 
   constructor() {
     this.results = [];
+    this.isSorted = false;
     this.queryPrefix = `https://api.themoviedb.org/3/search/movie?api_key=98c44624a954b0a3f8844a5f82194c8f&query=`;
-
     this.init();
   }
 
   init() {
     this.form = document.querySelector('.search-form');
+    this.header = document.querySelector('.table__header');
     this.counterElement = document.querySelector('.counter');
     this.counterValueElement = document.querySelector('.counter__value');
     this.main = document.querySelector('.main');
     this.tbody = document.querySelector('.data-table tbody');
 
     this.form.addEventListener('submit', this.submitHandler.bind(this));
+    this.header.addEventListener('click', this.clickHandler.bind(this));
   }
 
   processData({results}) {
     this.counterElement.classList.remove('hidden');
-    this.results = results;
+    this.results = this.filterSequence(results);
     this.updateTable(this.results);
   }
 
-  addTableRow(...rowData) {
-    const row = document.createElement('tr');
-    rowData.forEach(value => {
-      const td = document.createElement('td');
-      td.innerText = value;
-      row.appendChild(td);
+  filterObject({id, title, original_language:language, popularity, vote_count:votes, vote_average:rating, release_date:releaseDate}) {
+    return {
+      id,
+      title,
+      language,
+      popularity,
+      votes,
+      rating,
+      releaseDate
+    }
+  }
+
+  filterSequence(dataSequence) {
+    return dataSequence.map(this.filterObject);
+  }
+
+  sortSequenceByFieldName(sequence, field, reverse) {
+    const sorted = sequence.sort((left, right) => {
+
+      let [leftVal, rightVal] = [left[field], right[field]];
+
+      // for lexical comparison
+      leftVal = (typeof leftVal === 'string') ? leftVal.toLowerCase() : leftVal;
+      rightVal = (typeof rightVal === 'string') ? rightVal.toLowerCase() : rightVal;
+
+      if (reverse) {
+        return (leftVal < rightVal) ? 1 : (leftVal > rightVal) ? -1 : 0;
+      }
+
+      return  (leftVal > rightVal) ? 1 : ((leftVal < rightVal) ? -1 : 0);
     });
+
+    return sorted;
+  }
+
+  addTableRow(rowData) {
+    const row = document.createElement('tr');
+
+    for(const key in rowData) {
+      const td = document.createElement('td');
+      td.innerText = rowData[key];
+      row.appendChild(td);
+    }
 
     this.tbody.appendChild(row);
   }
 
-  cleanTable() {
+  clearTable() {
     this.tbody.innerHTML = '';
   }
 
   updateTable(dataSequence) {
-    this.cleanTable();
+    this.clearTable();
     this.counterValueElement.innerText = dataSequence.length;
 
-    dataSequence.forEach(
-      ({id,
-        title,
-        original_language:language,
-        popularity,
-        vote_count:votes,
-        vote_average:rating,
-        release_date:releaseDate
-      }) => {
-        this.addTableRow(id, title, language, popularity,  votes, rating, releaseDate);
-      }
-    );
+    dataSequence.forEach(data => {
+      this.addTableRow(data)
+    });
   }
 
   submitHandler(event) {
@@ -69,6 +98,14 @@ class MovieFinder {
     }).then(this.processData.bind(this));
   };
 
+  clickHandler(event) {
+    const target = event.target;
+    const key = target.getAttribute('data-key');
+    const sorted = this.sortSequenceByFieldName(this.results,key,this.isSorted);
+    this.updateTable(sorted);
+    target.classList.add()
+    this.isSorted = !this.isSorted;
+  }
 }
 
 new MovieFinder();
