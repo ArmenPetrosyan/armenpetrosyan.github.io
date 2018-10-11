476853c0686e5721a100525a96edfe50936b73f9
ArmenPetrosyan <thebraincrasher@gmail.com>
2018-10-11 19:18:26 +0300

    NEW: mobile

---
+++ build/precache-manifest.662393a9a2353609d189479f3a40b093.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c093e9d57bd51d5c8b2f",
+    "url": "/static/js/main.c093e9d5.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "c093e9d57bd51d5c8b2f",
+    "url": "/static/css/main.391ce5d9.chunk.css"
+  },
+  {
+    "revision": "4459f5c36c2d849baf0906c06606e60a",
+    "url": "/index.html"
+  }
+];
