06418b18bcbc124ff7f1bb47c60657ca08d525ff
ArmenPetrosyan <thebraincrasher@gmail.com>
2018-10-11 18:20:39 +0300

    NEW: markup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="App__header">
+          <div className="App__navigation">
+            <nav className="App__navigation-list">
+              <a className="App__navigation-list-item">about</a>
+              <a className="App__navigation-list-item">projects</a>
+              <a className="App__navigation-list-item">next</a>
+              <a className="App__navigation-list-item">contact me</a>
+            </nav>
+            <div className="App__logo">
+              <span className="App__logo_main">Arnie’s </span> showcase
+            </div>
+          </div>
+          <div className="App__welcome">
+            Arnie. <span className="App__welcome_highlight">Make frontend.</span> Love UX.
+          </div>
         </header>
+        <section className="App__section">
+          <h1 className="App__quote">Making Frontend <br/> Great Again</h1>
+          <div className="App__call-to-action">
+            <button className="App__call-to-action__button">Get my CV</button>
+            <div className='App__call-to-action__notice'>For your old-school recruitment specialist</div>
+          </div>
+        </section>
       </div>
     );
   }
